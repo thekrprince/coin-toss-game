38f56b322762d8d9fce2f0dd78383dc7b3809b3d
princekr96 <princekr700@gmail.com>
2021-06-24 23:49:48 +0530

    Created variable for query selector in JS file

---
+++ main.js
@@ -0,0 +1,7 @@
+var playerSelected = document.querySelector('.p-sel');
+var computerSelected = document.querySelector('.c-sel');
+var coinImg = document.querySelector('.coin-img');
+var playerScore = document.querySelector('.p-score');
+var computerScore = document.querySelector('.c-score');
+var headsBtn = document.querySelector('.heads-button');
+var tailsBtn = document.querySelector('.tails-button');
