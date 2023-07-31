e64971bc0e6454523c9afa23228c40c875783398
princekr96 <princekr700@gmail.com>
2023-07-31 12:18:23 +0530

    updated the game with restart logic

--- main.js
+++ main.js
@@ -1,26 +1,28 @@
-var playerSelected = document.querySelector('.p-sel');
-var computerSelected = document.querySelector('.c-sel');
-var coinImg = document.querySelector('#image');
-var playerScore = document.querySelector('.p-score');
-var computerScore = document.querySelector('.c-score');
-var winner = document.querySelector('.winner');
-var buttons = document.querySelectorAll('button');
+var playerSelected = document.querySelector(".p-sel");
+var computerSelected = document.querySelector(".c-sel");
+var coinImg = document.querySelector("#image");
+var playerScore = document.querySelector(".p-score");
+var computerScore = document.querySelector(".c-score");
+var winner = document.querySelector(".winner");
+var playButtons = document.querySelectorAll(".play-button");
+var restartButton = document.querySelector(".restart-button");
+var selectionTitle = document.querySelector(".selection");
 
 var userScore = 0;
 var machineScore = 0;
 
 function displaySelections(user, computer) {
-  if (user === 'heads') {
-    playerSelected.style.color = 'green';
+  if (user === "heads") {
+    playerSelected.style.color = "green";
   }
-  if (user === 'tails') {
-    playerSelected.style.color = 'blue';
+  if (user === "tails") {
+    playerSelected.style.color = "blue";
   }
-  if (computer === 'heads') {
-    computerSelected.style.color = 'green';
+  if (computer === "heads") {
+    computerSelected.style.color = "green";
   }
-  if (computer === 'tails') {
-    computerSelected.style.color = 'blue';
+  if (computer === "tails") {
+    computerSelected.style.color = "blue";
   }
   playerSelected.innerHTML = `${user}`;
   computerSelected.innerHTML = `${computer}`;
@@ -52,32 +54,38 @@ function scoreCalculation(random, userPick, computerPick) {
   } else if (machineScore === 5) {
     winner.innerHTML = `<h3>Computer Wins!!!</h3>`;
   }
+
+  if (userScore === 5 || machineScore === 5) {
+    playButtons.forEach((button) => button.classList.add("hide"));
+    restartButton.classList.remove("hide");
+    selectionTitle.classList.add("hide");
+  }
 }
 
-buttons.forEach(function (button) {
-  button.addEventListener('click', function (e) {
+playButtons.forEach(function (button) {
+  button.addEventListener("click", function (e) {
     const random = Math.round(Math.random());
     const computerPick = Math.round(Math.random());
 
     let computerSelection;
 
     if (computerPick === 1) {
-      computerSelection = 'heads';
+      computerSelection = "heads";
     } else {
-      computerSelection = 'tails';
+      computerSelection = "tails";
     }
 
     const userSelection = e.target.innerText;
     let userPick;
 
-    if (userSelection === 'heads') {
+    if (userSelection === "heads") {
       userPick = 1;
     } else {
       userPick = 0;
     }
 
     //  Spin the coin
-    coinImg.classList.add('animate');
+    coinImg.classList.add("animate");
 
     displayImageRandom(random);
 
@@ -87,7 +95,20 @@ buttons.forEach(function (button) {
     setTimeout(() => {
       scoreCalculation(random, userPick, computerPick);
 
-      coinImg.classList.remove('animate');
-    }, 3000);
+      coinImg.classList.remove("animate");
+    }, 1000);
   });
+});
+
+restartButton.addEventListener("click", function (e) {
+  userScore = 0;
+  machineScore = 0;
+  playButtons.forEach((button) => button.classList.remove("hide"));
+  restartButton.classList.add("hide");
+  selectionTitle.classList.remove("hide");
+  winner.innerHTML = ``;
+  playerScore.textContent = userScore;
+  computerScore.textContent = machineScore;
+  playerSelected.innerHTML = ``;
+  computerSelected.innerHTML = ``;
 });
